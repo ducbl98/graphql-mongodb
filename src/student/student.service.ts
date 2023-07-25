c481c9ed53939a3b92236f88e196f818df4558ce
Elcarim1802 <khiem.ha@bnksolution.com>
2023-07-25 18:06:58 +0700

    first commit

---
+++ src/student/student.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { In, MongoRepository, Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Student } from './student.entity';
+import { CreateStudentInput } from './student.input ';
+import { FindOperators } from 'mongodb';
+
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Student)
+    private studentRepository: MongoRepository<Student>,
+  ) {}
+
+  async getStudents(): Promise<Student[]> {
+    return this.studentRepository.find();
+  }
+
+  async getStudent(id: string): Promise<Student> {
+    return this.studentRepository.findOne({ where: { id } });
+  }
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+
+    return this.studentRepository.save(student);
+  }
+
+  async getManyStudents(studentIds: string[]): Promise<Student[]> {
+    return this.studentRepository.find({
+      where: {
+        id: {
+          $in: [...studentIds],
+        },
+      },
+    });
+  }
+}
